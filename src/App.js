1e6cdef37780c213dcf6805f2f031fe77c9cb6e8
sashank <sashanlt4@gmail.com>
2023-10-16 14:36:48 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import './App.css';
-
+import {useState} from 'react';
+import Tick from './images/icon-success.svg'
 function App() {
-  return (
+
+  const[email,setEmail] = useState('');
+  const[thank, setThank] = useState(false);
+  const[error,setError]=useState(false);
+
+  const handleClick = () =>{
+    setEmail('');
+    setThank(false);
+  }
+
+
+  const handleSubmit = () =>{
+   
+    if(email) {
+      setThank(true);
+    }
+    else{
+      setError(true);
+    }
+  }
+  return (thank)?(
+   
+    <div className='thanks-message'>
+      <img src={Tick} alt="tick"/>
+      <h1> Thanks for Subscribing</h1>
+      <p>A confirmation email is sent to <span> {email}</span>. Please open it and click the button inside to confirm the subsciption</p>
+      <button type='submit' onClick={() => handleClick()}>Dismiss message</button>
+      
+    </div>
+
+):(
     <div className="App">
       <div className='left'></div>
       <div className='right'>
@@ -13,12 +44,16 @@ function App() {
           <li>And much more!</li>
         </ul>
 
-        <form>
+        <form onSubmit={()=>handleSubmit()}>
           <label>Email address</label>
-          <input type='email' placeholder='email@company.com' />
-          <button> Subscribe to monthly newsletter</button>
+          <input type='email' placeholder='email@company.com' onChange={(e)=>setEmail(e.target.value)} value={email}/>
+          <button type='submit'> Subscribe to monthly newsletter</button>
+          {error?
+          <label className='error-message'>Enter valild email</label>: ""}
         </form>
       </div>
+
+      
     </div>
   );
 }
